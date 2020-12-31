14dd27ab1c29d97e4d081fb286ce2d4dcea54e9b
เมธาวี โพธิ์ไข่ <topfee1234@gmail.com>
2020-12-31 16:37:07 +0700

    edit home view

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> | 
     <router-link to="/skill">Skill</router-link> | 
     <router-link to="/workhistory">WorkHistory</router-link>
   </div>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,33 @@
 <template>
     <div id="home">
-        <img class="imgprofile" alt="Profile logo" src="../assets/profile.png">
-        <h1>Mr.Methawi Phokhai</h1>
+        <br />
+        <img class="imgprofile" :src="require(`../assets/profile.png`)">
+        <h1 class="fontstyle linespce" style="font-size:80px; margin-top:-10px;">Mr.Methawi Phokhai</h1>
+        <h2 class="fontstyle linespce" style="font-size:60px; margin-top:-70px">Web Developer</h2>
+        <br />
+        <div style="margin-top:-50px;">
+            <a href="https://github.com/phokhai"><img class="imgicon" :src="require(`../assets/github.svg`)"></a>&emsp;&emsp;
+            <a href="https://www.linkedin.com/in/methawiphokhai"><img class="imgicon" :src="require(`../assets/linkedin.png`)"></a>
+        </div>
     </div>
 </template>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Darker+Grotesque&display=swap');
+
 .imgprofile {
-    width: 240;
-    height: 250;
+    width: 240px;
+    height: 250px;
     border-radius: 50%;
 }
+
+.imgicon {
+    width: 40px;
+    height: 40px;
+}
+
+.fontstyle {
+    font-family: 'Darker Grotesque', sans-serif;
+}
+
 </style>
